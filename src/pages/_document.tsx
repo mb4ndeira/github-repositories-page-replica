3cd8322b4487f5c24d1be822016888a772d8ca56
mb4ndeira <mbandeira.cs@gmail.com>
2021-06-04 11:29:15 -0300

    feat: add fonts preload

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel="preload" href="/fonts/SegoeUI/SegoeUI-400.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/SegoeUI/SegoeUI-Semilight-350.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/SegoeUI/SegoeUI-Semibold-600.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/SegoeUI/SegoeUI-Gras-700.ttf" as="font" crossOrigin="" />
                 </Head>
                 <body>
                     <Main />
