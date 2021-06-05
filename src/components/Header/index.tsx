12a1ee11f23db4644a86f7d20f17400cc7117d4b
mb4ndeira <mbandeira.cs@gmail.com>
2021-06-04 23:19:27 -0300

    feat: add search bar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+import { BiChevronDown, BiChevronDown as DownIcon } from 'react-icons/bi'
+
+import SearchBar from '../SearchBar'
+
+import styles from './header.module.scss'
+
+interface HeaderProps {
+    keyPressed?: string;
+}
+
+export default function Header({ keyPressed }: HeaderProps) {
+    return (
+        <header className={styles.header}>
+            <div>
+                <img src="/images/github-logo.png" alt="none" />
+                <span>Why Github? <DownIcon className={styles.icon} /></span>
+                <span>Team</span>
+                <span>Enterprise</span>
+                <span>Marketplace</span>
+                <span>Pricing <DownIcon className={styles.icon} /></span>
+            </div>
+            <div>
+                {keyPressed === '/' ?
+                    <SearchBar active={true} />
+                    :
+                    <SearchBar active={false} />
+                }
+                <button>Sign in</button>
+                <button>Sign up</button>
+            </div>
+        </header>
+    )
+}
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,36 @@
+import React, { useState, useRef, useEffect } from 'react';
+
 import styles from './search-bar.module.scss'
 
-export default function SearchBar() {
+interface SearchBarProps {
+    active?: boolean;
+}
+
+export default function SearchBar({ active }: SearchBarProps) {
+    const [isActive, setIsActive] = useState(active)
+
+    const input = useRef(null)
+
+    useEffect(() => {
+        setIsActive(active)
+        if (!isActive) return
+
+        input.current.focus()
+    }, [active])
+
     return (
         <div className={styles.searchBar}>
-            <input type="text" placeholder="Search" />
-            <span >/</span>
+            <input
+                type="text" placeholder="Search"
+                onFocus={() => { if (!active) setIsActive(true) }}
+                onBlur={() => setIsActive(false)}
+                ref={input}
+            />
+            {!isActive &&
+                <span>
+                    <p>/</p>
+                </span>
+            }
         </div >
     )
 }
