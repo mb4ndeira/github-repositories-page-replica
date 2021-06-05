35570f6e3385d473736248222b02aa23e7d3bdac
mb4ndeira <mbandeira.cs@gmail.com>
2021-06-05 18:07:23 -0300

    feat: connect to github API

--- src/components/Repositories/Repository/index.tsx
+++ src/components/Repositories/Repository/index.tsx
@@ -4,33 +4,52 @@ import { VscRepoForked as Fork } from 'react-icons/vsc'
 
 import styles from './repository.module.scss'
 
-interface RepositoryProps {
-    tecnologyColor?: string;
+interface IRepositoryData {
+    name: string;
+    language: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    updated_at: string;
 }
 
-export default function Repository({ tecnologyColor = '#007acc' }: RepositoryProps) {
+interface RepositoryProps {
+    tecnologyColor?: string;
+    repositoryData: IRepositoryData;
+}
+
+export default function Repository({ tecnologyColor = '#007acc', repositoryData }: RepositoryProps) {
+    const {
+        name,
+        language,
+        description,
+        stargazers_count: stars,
+        forks_count: forks,
+        updated_at: date
+    } = repositoryData
+
     return (
         <div className={styles.repository}>
             <div>
                 <div>
-                    <h3><Link href="javascript:"><a> frontend-challenge</a></Link></h3>
-                    <p>Forked from FotonTech/frontend-challenge</p>
+                    <h3><Link href="javascript:"><a>{name}</a></Link></h3>
+                    <p>{description}</p>
                 </div>
                 <section>
                     <div className={styles.tecnology}>
                         <span style={{ background: tecnologyColor }}></span>
-                        <p>Typescript</p>
+                        <p>{language}</p>
                     </div>
                     <div className={styles.stars}>
                         <Star className={styles.icon} />
-                        <p>2</p>
+                        <p>{stars}</p>
                     </div>
                     <div className={styles.forks}>
                         <Fork className={styles.icon} />
-                        <p>2</p>
+                        <p>{forks}</p>
                     </div>
                     <div className={styles.date}>
-                        <p>Updated on 9 Apr</p>
+                        <p>{date}</p>
                     </div>
                 </section>
             </div>
--- src/components/Repositories/index.tsx
+++ src/components/Repositories/index.tsx
@@ -1,13 +1,36 @@
-import styles from './repositories.module.scss'
+import React, { useState, useEffect } from 'react'
 
 import Header from './Header'
-import Repository from './Repository'
+import RepositoryItem from './Repository'
+
+import styles from './repositories.module.scss'
+
+interface IRepository {
+    id: number;
+    name: string;
+    language: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    updated_at: string;
+}
 
 export default function Repositories() {
+    const [repositories, setRepositories] = useState<IRepository[]>([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/mb4ndeira/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
     return (
         <div className={styles.repositories}>
             <Header />
-            <Repository />
+            {repositories.map(repository => <RepositoryItem
+                key={repository.id}
+                repositoryData={repository}
+            />)}
         </div >
     )
 }
